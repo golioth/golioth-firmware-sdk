c5c176007a11b9d7ddbc7e0f3793fd3945621065
Nick Miller <nick@golioth.io>
2022-11-04 14:17:19 -0700

    wifi.h: add missing includes
    With minimal applications, including wifi.h can result in
    compiler errors due to missing bool and stdint types.
    
    Signed-off-by: Nick Miller <nick@golioth.io>

--- examples/esp_idf/common/wifi.h
+++ examples/esp_idf/common/wifi.h
@@ -5,6 +5,9 @@
  */
 #pragma once
 
+#include <stdbool.h>
+#include <stdint.h>
+
 void wifi_init(const char* ssid, const char* password);
 void wifi_wait_for_connected(void);
 bool wifi_wait_for_connected_with_timeout(uint32_t timeout_s);
