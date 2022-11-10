ff9dcc5e0a36ce67a2cf5a512344dbc4aeb3f73e
Nick Miller <nick@golioth.io>
2022-11-10 10:49:32 -0800

    port,mtb: override GLTH_LOGX to avoid 64-bit num
    The basic ModusToolbox libc does not have good support for
    use 64-bit formatters (like PRIu64) in the printf family
    of functions. MTB does support it with newlib, but that
    is a larger library that most MTB examples/applications
    don't use, so we should avoid it too.
    
    For the MTB port, we override GLTH_LOGX to use 32-bit timestamps.
    This is necessary, otherwise the log output become 30% garbled
    mess.
    
    Signed-off-by: Nick Miller <nick@golioth.io>

--- port/modus_toolbox/golioth_port_config.h
+++ port/modus_toolbox/golioth_port_config.h
@@ -1,3 +1,15 @@
 #pragma once
 
-// No port config necessary
+// Same as GLTH_LOGX, but with 32-bit timestamp (PRIu32),
+// since MTB built-in C lib doesn't support 64-bit formatters.
+#define GLTH_LOGX(COLOR, LEVEL, LEVEL_STR, TAG, ...) \
+    do { \
+        if ((LEVEL) <= golioth_debug_get_log_level()) { \
+            uint32_t now_ms = (uint32_t)golioth_time_millis(); \
+            printf(COLOR "%s (%" PRIu32 ") %s: ", LEVEL_STR, now_ms, TAG); \
+            printf(__VA_ARGS__); \
+            golioth_debug_printf(now_ms, LEVEL, TAG, __VA_ARGS__); \
+            printf("%s", LOG_RESET_COLOR); \
+            puts(""); \
+        } \
+    } while (0)
