e33e0e68257243648802593c1c45409441f2047c
Nick Miller <nick@golioth.io>
2023-01-04 15:40:11 -0800

    wifi: fix potential strncpy size issue
    strncpy will not add a NULL terminator if dest is longer
    than source, so we need to make sure to reserve the last byte
    of the buffer for NULL.
    
    Signed-off-by: Nick Miller <nick@golioth.io>

--- examples/esp_idf/common/wifi.c
+++ examples/esp_idf/common/wifi.c
@@ -75,8 +75,8 @@ void wifi_init(const char* ssid, const char* password) {
 
     wifi_config_t wifi_config = {};
     wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
-    strncpy((char*)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid));
-    strncpy((char*)wifi_config.sta.password, password, sizeof(wifi_config.sta.password));
+    strncpy((char*)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid) - 1);
+    strncpy((char*)wifi_config.sta.password, password, sizeof(wifi_config.sta.password) - 1);
     esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
 
     ESP_ERROR_CHECK(esp_wifi_start());
