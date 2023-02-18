2e38070e8d75578f33a5ed2716faf0fa9181078a
Nick Miller <nick@golioth.io>
2023-02-17 17:37:46 -0800

    port/esp-idf: remove fw_update_header_valid()
    With the streaming-based approach of the block processor, sometimes the
    full IDF header is not available in the first chunk.
    
    This check is not strictly necessary, since there is a validation step
    at the end (esp_ota_end()) which would catch any format/checksum errors.
    
    Fixing the code so we wait long enough for the full header to be received
    before calling fw_update_header_valid() would complicate things, and
    given that the check isn't necessary, it's preferred to simply delete it.
    
    Signed-off-by: Nick Miller <nick@golioth.io>

--- port/esp_idf/fw_update_esp_idf.c
+++ port/esp_idf/fw_update_esp_idf.c
@@ -12,40 +12,6 @@
 static esp_ota_handle_t _update_handle;
 static const esp_partition_t* _update_partition;
 
-static bool fw_update_header_valid(const uint8_t* bytes, size_t nbytes) {
-    size_t header_size = sizeof(esp_image_header_t) + sizeof(esp_image_segment_header_t)
-            + sizeof(esp_app_desc_t);
-    assert(nbytes >= header_size);
-
-    esp_app_desc_t new_app_info;
-    memcpy(&new_app_info,
-           &bytes[sizeof(esp_image_header_t) + sizeof(esp_image_segment_header_t)],
-           sizeof(esp_app_desc_t));
-
-    esp_app_desc_t running_app_info;
-    esp_ota_get_partition_description(esp_ota_get_running_partition(), &running_app_info);
-
-    esp_app_desc_t invalid_app_info;
-    const esp_partition_t* last_invalid_app = esp_ota_get_last_invalid_partition();
-    esp_ota_get_partition_description(last_invalid_app, &invalid_app_info);
-
-    // check current version with last invalid partition
-    if (last_invalid_app) {
-        if (memcmp(invalid_app_info.version, new_app_info.version, sizeof(new_app_info.version))
-            == 0) {
-            GLTH_LOGW(TAG, "New version is the same as invalid version.");
-            GLTH_LOGW(
-                    TAG,
-                    "Previously, there was an attempt to launch the firmware with %s version, but it failed.",
-                    invalid_app_info.version);
-            GLTH_LOGW(TAG, "The firmware has been rolled back to the previous version.");
-            return false;
-        }
-    }
-
-    return true;
-}
-
 bool fw_update_is_pending_verify(void) {
     const esp_partition_t* running = esp_ota_get_running_partition();
     esp_ota_img_states_t ota_state;
@@ -77,10 +43,6 @@ golioth_status_t fw_update_handle_block(
     esp_err_t err = ESP_OK;
 
     if (offset == 0) {
-        if (!fw_update_header_valid(block, block_size)) {
-            return GOLIOTH_ERR_FAIL;
-        }
-
         _update_partition = esp_ota_get_next_update_partition(NULL);
         assert(_update_partition);
         GLTH_LOGI(
