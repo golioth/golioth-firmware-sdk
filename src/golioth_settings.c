54a720db64f23ff6016a294b8a08697c825f3907
Marcin Niestroj <m.niestroj@emb.dev>
2023-06-13 15:42:50 +0200

    settings: convert from cJSON to zcbor
    Encoding log entries into CBOR allows to reduce payload size and make use
    of more compact libraries to generate such payload.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- src/golioth_settings.c
+++ src/golioth_settings.c
@@ -10,8 +10,11 @@
 #include "golioth_coap_client.h"
 #include "golioth_statistics.h"
 #include "golioth_debug.h"
-#include <cJSON.h>
+#include "zcbor_utils.h"
+#include <errno.h>
 #include <math.h>  // modf
+#include <zcbor_decode.h>
+#include <zcbor_encode.h>
 
 // Example settings request from cloud:
 //
@@ -41,11 +44,48 @@ LOG_TAG_DEFINE(golioth_settings);
 #define SETTINGS_PATH_PREFIX ".c/"
 #define SETTINGS_STATUS_PATH "status"
 
-static void add_error_to_array(cJSON* array, const char* key, golioth_settings_status_t code) {
-    cJSON* error = cJSON_CreateObject();
-    cJSON_AddStringToObject(error, "setting_key", key);
-    cJSON_AddNumberToObject(error, "error_code", (double)code);
-    cJSON_AddItemToArray(array, error);
+#define GOLIOTH_SETTINGS_MAX_RESPONSE_LEN 256
+#define GOLIOTH_SETTINGS_MAX_NAME_LEN 63 /* not including NULL */
+
+struct settings_response {
+    zcbor_state_t zse[1 /* num_backups */ + 2];
+    uint8_t buf[GOLIOTH_SETTINGS_MAX_RESPONSE_LEN];
+    size_t num_errors;
+    golioth_client_t client;
+};
+
+static void response_init(struct settings_response* response, golioth_client_t client) {
+    memset(response, 0, sizeof(*response));
+
+    response->client = client;
+
+    zcbor_new_encode_state(
+            response->zse, ARRAY_SIZE(response->zse), response->buf, sizeof(response->buf), 1);
+
+    /* Initialize the map */
+    zcbor_map_start_encode(response->zse, ZCBOR_MAX_ELEM_COUNT);
+}
+
+static void add_error_to_response(
+        struct settings_response* response,
+        const char* key,
+        golioth_settings_status_t code) {
+    if (response->num_errors == 0) {
+        zcbor_tstr_put_lit(response->zse, "errors");
+        zcbor_list_start_encode(response->zse, ZCBOR_MAX_ELEM_COUNT);
+    }
+
+    zcbor_map_start_encode(response->zse, 2);
+
+    zcbor_tstr_put_lit(response->zse, "setting_key");
+    zcbor_tstr_put_term(response->zse, key);
+
+    zcbor_tstr_put_lit(response->zse, "error_code");
+    zcbor_int64_put(response->zse, code);
+
+    zcbor_map_end_encode(response->zse, 2);
+
+    response->num_errors++;
 }
 
 static golioth_setting_t* find_registered_setting(golioth_settings_t* gsettings, const char* key) {
@@ -61,6 +101,199 @@ static golioth_setting_t* find_registered_setting(golioth_settings_t* gsettings,
     return NULL;
 }
 
+static int finalize_and_send_response(
+        golioth_client_t client,
+        struct settings_response* response,
+        int64_t version) {
+    bool ok;
+
+    /*
+     * If there were errors, then the "errors" array is still open,
+     * so we need to close it.
+     */
+    if (response->num_errors > 0) {
+        ok = zcbor_list_end_encode(response->zse, SIZE_MAX);
+        if (!ok) {
+            return -ENOMEM;
+        }
+    }
+
+    /* Set version */
+    ok = zcbor_tstr_put_lit(response->zse, "version") && zcbor_int64_put(response->zse, version);
+    if (!ok) {
+        return -ENOMEM;
+    }
+
+    /* Close the root map */
+    ok = zcbor_map_end_encode(response->zse, 1);
+    if (!ok) {
+        return -ENOMEM;
+    }
+
+    GLTH_LOG_BUFFER_HEXDUMP(
+            TAG,
+            response->buf,
+            response->zse->payload - response->buf,
+            GOLIOTH_DEBUG_LOG_LEVEL_DEBUG);
+
+    golioth_coap_client_set(
+            client,
+            SETTINGS_PATH_PREFIX,
+            "status",
+            COAP_MEDIATYPE_APPLICATION_CBOR,
+            response->buf,
+            response->zse->payload - response->buf,
+            NULL,
+            NULL,
+            false,
+            GOLIOTH_WAIT_FOREVER);
+
+    return 0;
+}
+
+static int settings_decode(zcbor_state_t* zsd, void* value) {
+    struct settings_response* settings_response = value;
+    golioth_client_t client = settings_response->client;
+    struct zcbor_string label;
+    golioth_settings_status_t setting_status = GOLIOTH_SETTINGS_SUCCESS;
+    golioth_settings_t* gsettings = golioth_coap_client_get_settings(client);
+    bool ok;
+
+    if (zcbor_nil_expect(zsd, NULL)) {
+        /* No settings are set */
+        return -ENOENT;
+    }
+
+    ok = zcbor_map_start_decode(zsd);
+    if (!ok) {
+        GLTH_LOGW(TAG, "Did not start CBOR list correctly");
+        return -EBADMSG;
+    }
+
+    while (!zcbor_list_or_map_end(zsd)) {
+        /* Handle item */
+        ok = zcbor_tstr_decode(zsd, &label);
+        if (!ok) {
+            GLTH_LOGE(TAG, "Failed to get label");
+            return -EBADMSG;
+        }
+
+        char key[GOLIOTH_SETTINGS_MAX_NAME_LEN + 1] = {};
+
+        /* Copy setting label/name and ensure it's NULL-terminated */
+        memcpy(key, label.value, MIN(GOLIOTH_SETTINGS_MAX_NAME_LEN, label.len));
+
+        bool data_type_valid = true;
+
+        zcbor_major_type_t major_type = ZCBOR_MAJOR_TYPE(*zsd->payload);
+
+        GLTH_LOGD(TAG, "key = %s, major_type = %d", key, major_type);
+
+        const golioth_setting_t* registered_setting = find_registered_setting(gsettings, key);
+        if (!registered_setting) {
+            add_error_to_response(settings_response, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
+
+            ok = zcbor_any_skip(zsd, NULL);
+            if (!ok) {
+                GLTH_LOGE(TAG, "Failed to skip unsupported type");
+                return -EBADMSG;
+            }
+
+            continue;
+        }
+
+        switch (major_type) {
+            case ZCBOR_MAJOR_TYPE_TSTR: {
+                struct zcbor_string str;
+
+                if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_STRING) {
+                    data_type_valid = false;
+                    break;
+                }
+
+                ok = zcbor_tstr_decode(zsd, &str);
+                if (!ok) {
+                    data_type_valid = false;
+                    break;
+                }
+
+                setting_status = registered_setting->string_cb(
+                        (char*)str.value, str.len, registered_setting->cb_arg);
+                break;
+            }
+            case ZCBOR_MAJOR_TYPE_PINT:
+            case ZCBOR_MAJOR_TYPE_NINT: {
+                int64_t value;
+
+                if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_INT) {
+                    data_type_valid = false;
+                    break;
+                }
+
+                ok = zcbor_int64_decode(zsd, &value);
+                if (!ok) {
+                    data_type_valid = false;
+                    break;
+                }
+
+                setting_status =
+                        registered_setting->int_cb((int32_t)value, registered_setting->cb_arg);
+                break;
+            }
+            case ZCBOR_MAJOR_TYPE_SIMPLE: {
+                bool value_bool;
+                double value_double;
+
+                if (zcbor_float_decode(zsd, &value_double)) {
+                    if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_FLOAT) {
+                        data_type_valid = false;
+                        break;
+                    }
+
+                    setting_status = registered_setting->float_cb(
+                            (float)value_double, registered_setting->cb_arg);
+                } else if (zcbor_bool_decode(zsd, &value_bool)) {
+                    if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_BOOL) {
+                        data_type_valid = false;
+                        break;
+                    }
+
+                    setting_status =
+                            registered_setting->bool_cb(value_bool, registered_setting->cb_arg);
+                } else {
+                    break;
+                }
+                break;
+            }
+            default:
+                data_type_valid = false;
+                break;
+        }
+
+        if (data_type_valid) {
+            if (setting_status != GOLIOTH_SETTINGS_SUCCESS) {
+                add_error_to_response(settings_response, key, setting_status);
+            }
+        } else {
+            add_error_to_response(settings_response, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
+
+            ok = zcbor_any_skip(zsd, NULL);
+            if (!ok) {
+                GLTH_LOGE(TAG, "Failed to skip unsupported type");
+                return -EBADMSG;
+            }
+        }
+    }
+
+    ok = zcbor_map_end_decode(zsd);
+    if (!ok) {
+        GLTH_LOGW(TAG, "Did not end CBOR list correctly");
+        return -EBADMSG;
+    }
+
+    return 0;
+}
+
 static void on_settings(
         golioth_client_t client,
         const golioth_response_t* response,
@@ -68,154 +301,35 @@ static void on_settings(
         const uint8_t* payload,
         size_t payload_size,
         void* arg) {
-    if (payload_size == 0 || payload[0] != '{') {
-        // Ignore anything that is clearly not JSON
+    ZCBOR_STATE_D(zsd, 2, payload, payload_size, 1);
+    int64_t version;
+    struct settings_response settings_response;
+    struct zcbor_map_entry map_entries[] = {
+            ZCBOR_TSTR_LIT_MAP_ENTRY("settings", settings_decode, &settings_response),
+            ZCBOR_TSTR_LIT_MAP_ENTRY("version", zcbor_map_int64_decode, &version),
+    };
+    int err;
+
+    GLTH_LOG_BUFFER_HEXDUMP(TAG, payload, payload_size, GOLIOTH_DEBUG_LOG_LEVEL_DEBUG);
+
+    if (payload_size == 3 && payload[1] == 'O' && payload[2] == 'K') {
+        /* Ignore "OK" response received after observing */
         return;
     }
 
     GLTH_LOG_BUFFER_HEXDUMP(TAG, payload, min(64, payload_size), GOLIOTH_DEBUG_LOG_LEVEL_DEBUG);
 
-    cJSON* json = cJSON_ParseWithLength((const char*)payload, payload_size);
-    if (!json) {
-        GLTH_LOGE(TAG, "Failed to parse settings");
-        goto cleanup;
-    }
-    GSTATS_INC_ALLOC("on_settings_json");
+    response_init(&settings_response, client);
 
-    const cJSON* version = cJSON_GetObjectItemCaseSensitive(json, "version");
-    if (!version || !cJSON_IsNumber(version)) {
-        GLTH_LOGE(TAG, "Key version not found");
-        goto cleanup;
-    }
-
-    const cJSON* settings = cJSON_GetObjectItemCaseSensitive(json, "settings");
-    if (!settings) {
-        GLTH_LOGE(TAG, "Key settings not found");
-        goto cleanup;
-    }
-
-    // Create status report that we'll send back to Golioth after processing settings
-    cJSON* report = cJSON_CreateObject();
-    cJSON_AddNumberToObject(report, "version", version->valueint);  // copied from request
-    cJSON* errors = cJSON_AddArrayToObject(report, "errors");
-
-    golioth_settings_t* gsettings = golioth_coap_client_get_settings(client);
-
-    // Iterate over settings object and call callback for each setting
-    cJSON* setting = settings->child;
-    while (setting) {
-        const char* key = setting->string;
-
-        const golioth_setting_t* registered_setting = find_registered_setting(gsettings, key);
-        if (!registered_setting) {
-            add_error_to_array(errors, key, GOLIOTH_SETTINGS_KEY_NOT_RECOGNIZED);
-            goto next_setting;
+    err = zcbor_map_decode(zsd, map_entries, ARRAY_SIZE(map_entries));
+    if (err) {
+        if (err != -ENOENT) {
+            GLTH_LOGE(TAG, "Failed to parse tstr map");
         }
-
-        if (cJSON_IsString(setting)) {
-            if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_STRING) {
-                add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
-                goto next_setting;
-            }
-
-            assert(registered_setting->string_cb);
-            golioth_settings_status_t setting_status = registered_setting->string_cb(
-                    setting->valuestring, strlen(setting->valuestring), registered_setting->cb_arg);
-
-            if (setting_status != GOLIOTH_SETTINGS_SUCCESS) {
-                add_error_to_array(errors, key, setting_status);
-            }
-        } else if (cJSON_IsBool(setting)) {
-            if (registered_setting->type != GOLIOTH_SETTINGS_VALUE_TYPE_BOOL) {
-                add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
-                goto next_setting;
-            }
-
-            assert(registered_setting->bool_cb);
-            golioth_settings_status_t setting_status = registered_setting->bool_cb(
-                    (bool)setting->valueint, registered_setting->cb_arg);
-
-            if (setting_status != GOLIOTH_SETTINGS_SUCCESS) {
-                add_error_to_array(errors, key, setting_status);
-            }
-        } else if (cJSON_IsNumber(setting)) {
-            if (registered_setting->type == GOLIOTH_SETTINGS_VALUE_TYPE_FLOAT) {
-                assert(registered_setting->float_cb);
-
-                golioth_settings_status_t setting_status = registered_setting->float_cb(
-                        (float)setting->valuedouble, registered_setting->cb_arg);
-                if (setting_status != GOLIOTH_SETTINGS_SUCCESS) {
-                    add_error_to_array(errors, key, setting_status);
-                }
-            } else if (registered_setting->type == GOLIOTH_SETTINGS_VALUE_TYPE_INT) {
-                // Use modf to determine if this number is an int or a float
-                // TODO - is there a more efficient way to do this?
-                double int_part = 0;
-                double fractional_part = modf(setting->valuedouble, &int_part);
-                const double epsilon = 0.000001f;
-                if (fractional_part > epsilon) {
-                    // Registered type is integer, but received float
-                    add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
-                    goto next_setting;
-                }
-
-                // Verify min/max range
-                int32_t new_value = (int32_t)setting->valueint;
-                if ((new_value < registered_setting->int_min_val)
-                    || (new_value > registered_setting->int_max_val)) {
-                    add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_OUTSIDE_RANGE);
-                    goto next_setting;
-                }
-
-                assert(registered_setting->int_cb);
-                golioth_settings_status_t setting_status = registered_setting->int_cb(
-                        (int32_t)setting->valueint, registered_setting->cb_arg);
-
-                if (setting_status != GOLIOTH_SETTINGS_SUCCESS) {
-                    add_error_to_array(errors, key, setting_status);
-                }
-            } else {
-                // Received Number, but registered type is neither integer nor float
-                add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
-                goto next_setting;
-            }
-        } else {
-            GLTH_LOGW(TAG, "Setting with key %s has unknown type", key);
-            add_error_to_array(errors, key, GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID);
-            goto next_setting;
-        }
-
-    next_setting:
-        setting = setting->next;
+        return;
     }
 
-    // In case of no errors, the errors array must be omitted from the report.
-    if (cJSON_GetArraySize(errors) == 0) {
-        cJSON_DeleteItemFromObject(report, "errors");
-    }
-
-    // Serialize and send the status report
-    char* json_string = cJSON_PrintUnformatted(report);
-    GLTH_LOGD(TAG, "Sending status: %s", json_string);
-    golioth_coap_client_set(
-            client,
-            SETTINGS_PATH_PREFIX,
-            "status",
-            COAP_MEDIATYPE_APPLICATION_JSON,
-            (const uint8_t*)json_string,
-            strlen(json_string),
-            NULL,
-            NULL,
-            false,
-            GOLIOTH_WAIT_FOREVER);
-    golioth_sys_free(json_string);
-    cJSON_Delete(report);
-
-cleanup:
-    if (json) {
-        cJSON_Delete(json);
-        GSTATS_INC_FREE("on_settings_json");
-    }
+    finalize_and_send_response(client, &settings_response, version);
 }
 
 static void settings_lazy_init(golioth_client_t client) {
@@ -226,7 +340,7 @@ static void settings_lazy_init(golioth_client_t client) {
     }
 
     golioth_status_t status = golioth_coap_client_observe_async(
-            client, SETTINGS_PATH_PREFIX, "", COAP_MEDIATYPE_APPLICATION_JSON, on_settings, NULL);
+            client, SETTINGS_PATH_PREFIX, "", COAP_MEDIATYPE_APPLICATION_CBOR, on_settings, NULL);
 
     if (status != GOLIOTH_OK) {
         GLTH_LOGE(TAG, "Failed to observe settings");
