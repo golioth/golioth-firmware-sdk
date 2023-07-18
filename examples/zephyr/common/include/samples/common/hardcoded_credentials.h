f0388063de3a6935b3a11f97123919efcf72acfb
Mike Szczys <mike@golioth.io>
2023-07-18 14:25:00 -0500

    zephyr: common: add SAMPLE_COMMON and PSK handling
    * Add GOLIOTH_SAMPLE_COMMON
    * Add the `hardcoded_credentials` samples common helper from the Golioth
      Zephyr SDK.
    * add `hardcoded_credentials_get()` to return a `golioth_client_config_t`
      used to create a Golioth client
    * PSK or PKI (certificate auth) are returned automatically based on the
      `CONFIG_GOLIOTH_AUTH_METHOD_CERT` symbol
    * PKI is not yet implemented in the port. Placeholder code must be
      updated/replace when support is available
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ examples/zephyr/common/include/samples/common/hardcoded_credentials.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __GOLIOTH_INCLUDE_HARDCODED_CREDENTIALS_H__
+#define __GOLIOTH_INCLUDE_HARDCODED_CREDENTIALS_H__
+
+#include "golioth.h"
+
+golioth_client_config_t* hardcoded_credentials_get(void);
+
+#endif /* __GOLIOTH_INCLUDE_HARDCODED_CREDENTIALS_H__ */
