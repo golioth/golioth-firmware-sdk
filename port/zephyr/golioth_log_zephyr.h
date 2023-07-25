82c64a495aa01115e431865c3919f7d91aaa6a33
Sam Friedman <sam@golioth.io>
2023-07-25 17:01:09 -0400

    port/zephyr: Add Zephyr log backend
    Add a log backend for Zephyr that connects to the Golioth cloud logging
    module.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ port/zephyr/golioth_log_zephyr.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2023 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int log_backend_golioth_enable(void* client);
+int log_backend_golioth_disable(void* client);
