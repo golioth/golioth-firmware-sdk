1858221b0ecea7bb128e65dd455e8b412375fff6
Mike Szczys <mike@golioth.io>
2023-07-25 14:43:35 -0500

    examples/zephyr: add LightDB observe
    Observe a LightDB State path. Receive new data at boot and anytime that
    path changes on the cloud.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ examples/zephyr/lightdb/observe/src/main.c
@@ -0,0 +1,71 @@
+/*
+ * copyright (c) 2023 golioth, inc.
+ *
+ * spdx-license-identifier: apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(lightdb_observe, LOG_LEVEL_DBG);
+
+#include "golioth.h"
+#include <samples/common/hardcoded_credentials.h>
+#include <string.h>
+#include <zephyr/kernel.h>
+
+#include <samples/common/net_connect.h>
+
+#define APP_TIMEOUT_S 1
+
+golioth_client_t client;
+static K_SEM_DEFINE(connected, 0, 1);
+
+static void on_client_event(golioth_client_t client, golioth_client_event_t event, void* arg) {
+    bool is_connected = (event == GOLIOTH_CLIENT_EVENT_CONNECTED);
+    if (is_connected) {
+        k_sem_give(&connected);
+    }
+    LOG_INF("Golioth client %s", is_connected ? "connected" : "disconnected");
+}
+
+static void counter_observe_handler(
+        golioth_client_t client,
+        const golioth_response_t* response,
+        const char* path,
+        const uint8_t* payload,
+        size_t payload_size,
+        void* arg) {
+    if (response->status != GOLIOTH_OK) {
+        LOG_WRN("Failed to receive observed payload: %d", response->status);
+        return;
+    }
+
+    LOG_HEXDUMP_INF(payload, payload_size, "Counter (async)");
+
+    return;
+}
+
+int main(void) {
+    LOG_DBG("Start LightDB observe sample");
+
+    IF_ENABLED(CONFIG_GOLIOTH_SAMPLE_COMMON, (net_connect();))
+
+    /* Note: In production, you would provision unique credentials onto each
+     * device. For simplicity, we provide a utility to hardcode credentials as
+     * kconfig options in the samples.
+     */
+    golioth_client_config_t* client_config = hardcoded_credentials_get();
+
+    client = golioth_client_create(client_config);
+    golioth_client_register_event_callback(client, on_client_event, NULL);
+
+    k_sem_take(&connected, K_FOREVER);
+
+    /* Observe LightDB State Path */
+    golioth_lightdb_observe_async(client, "counter", counter_observe_handler, NULL);
+
+    while (true) {
+        k_sleep(K_SECONDS(5));
+    }
+
+    return 0;
+}
