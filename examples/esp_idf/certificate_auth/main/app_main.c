016ee267f4741ecd80f37ad5b4910b1b04d36352
Sam Friedman <sam@golioth.io>
2023-08-07 16:39:57 -0400

    cert: Move Server CA cert to src directory
    The Server CA cert is used by all ports, so this commit moves
    it from the samples/esp-idf directory to the src/ directory.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- examples/esp_idf/certificate_auth/main/app_main.c
+++ examples/esp_idf/certificate_auth/main/app_main.c
@@ -12,8 +12,8 @@
 
 #define TAG "certificate_auth"
 
-extern const uint8_t ca_pem_start[] asm("_binary_root_ca_pem_start");
-extern const uint8_t ca_pem_end[] asm("_binary_root_ca_pem_end");
+extern const uint8_t ca_pem_start[] asm("_binary_isrgrootx1_pem_start");
+extern const uint8_t ca_pem_end[] asm("_binary_isrgrootx1_pem_end");
 extern const uint8_t client_pem_start[] asm("_binary_client_pem_start");
 extern const uint8_t client_pem_end[] asm("_binary_client_pem_end");
 extern const uint8_t client_key_start[] asm("_binary_client_key_start");
