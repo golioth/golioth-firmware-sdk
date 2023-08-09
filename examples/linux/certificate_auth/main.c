1a82e6aed266c776f18b1162df6523895f0290ca
Sam Friedman <sam@golioth.io>
2023-08-09 13:41:50 -0400

    auth: use DER instead of PEM certificates
    DER certificaates are smaller and more efficient than PEM certificates.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- examples/linux/certificate_auth/main.c
+++ examples/linux/certificate_auth/main.c
@@ -5,7 +5,7 @@
 
 #define TAG "main"
 
-#define MAX_PEM_FILE_SIZE 2048
+#define MAX_DER_FILE_SIZE 2048
 
 static int read_file(const char* filepath, uint8_t* buffer, size_t buffer_size) {
     FILE* fp = fopen(filepath, "rb");
@@ -21,14 +21,14 @@ static int read_file(const char* filepath, uint8_t* buffer, size_t buffer_size) 
 }
 
 int main(void) {
-    uint8_t client_key_pem[MAX_PEM_FILE_SIZE];
-    uint8_t client_cert_pem[MAX_PEM_FILE_SIZE];
-    uint8_t root_ca_pem[MAX_PEM_FILE_SIZE];
+    uint8_t client_key_der[MAX_DER_FILE_SIZE];
+    uint8_t client_cert_der[MAX_DER_FILE_SIZE];
+    uint8_t root_ca_der[MAX_DER_FILE_SIZE];
 
-    size_t client_key_len = read_file("certs/client.key", client_key_pem, sizeof(client_key_pem));
+    size_t client_key_len = read_file("certs/client.key.der", client_key_der, sizeof(client_key_der));
     size_t client_cert_len =
-            read_file("certs/client.pem", client_cert_pem, sizeof(client_cert_pem));
-    size_t root_ca_len = read_file("root_ca.pem", root_ca_pem, sizeof(root_ca_pem));
+            read_file("certs/client.crt.der", client_cert_der, sizeof(client_cert_der));
+    size_t root_ca_len = read_file("isrgrootx1.der", root_ca_der, sizeof(root_ca_der));
     if ((client_key_len <= 0) || (client_cert_len <= 0) || (root_ca_len <= 0)) {
         return 1;
     }
@@ -37,11 +37,11 @@ int main(void) {
             .credentials = {
                     .auth_type = GOLIOTH_TLS_AUTH_TYPE_PKI,
                     .pki = {
-                            .ca_cert = root_ca_pem,
+                            .ca_cert = root_ca_der,
                             .ca_cert_len = root_ca_len,
-                            .public_cert = client_cert_pem,
+                            .public_cert = client_cert_der,
                             .public_cert_len = client_cert_len,
-                            .private_key = client_key_pem,
+                            .private_key = client_key_der,
                             .private_key_len = client_key_len,
                     }}};
 
