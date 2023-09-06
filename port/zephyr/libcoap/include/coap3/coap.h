b6c2030292881cc702d3fbf22ba99a1b9c0a7982
Daniel Mangum <georgedanielmangum@gmail.com>
2023-09-06 14:19:20 -0400

    Update libcoap header includes in Zephyr port
    Updates the list of included headers from the new libcoap version in the
    Zephyr port.
    
    Signed-off-by: Daniel Mangum <georgedanielmangum@gmail.com>

--- port/zephyr/libcoap/include/coap3/coap.h
+++ port/zephyr/libcoap/include/coap3/coap.h
@@ -8,9 +8,9 @@ extern "C" {
 #include "coap3/libcoap.h"
 
 #include "coap3/coap_forward_decls.h"
-#include "coap3/block.h"
 #include "coap3/coap_address.h"
 #include "coap3/coap_async.h"
+#include "coap3/coap_block.h"
 #include "coap3/coap_cache.h"
 #include "coap3/coap_debug.h"
 #include "coap3/coap_dtls.h"
@@ -20,13 +20,14 @@ extern "C" {
 #include "coap3/coap_mem.h"
 #include "coap3/coap_net.h"
 #include "coap3/coap_option.h"
+#include "coap3/coap_pdu.h"
 #include "coap3/coap_prng.h"
 #include "coap3/coap_str.h"
+#include "coap3/coap_resource.h"
 #include "coap3/coap_subscribe.h"
 #include "coap3/coap_time.h"
 #include "coap3/coap_uri.h"
-#include "coap3/pdu.h"
-#include "coap3/resource.h"
+#include "coap3/coap_ws.h"
 
 #ifdef __cplusplus
 }
