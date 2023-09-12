4482643563c2b8ea4d844685dd39ad7f1115b9b8
Sam Friedman <sam@golioth.io>
2023-09-11 21:13:32 -0400

    examples: zephyr: common: autoload settings
    Adds a module to automatically load settings from non-volatile storage
    before main() is run. This is a convenience function so the application
    does not have to call settings_load() and ensures that the settings
    are immediately available when the application starts.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ examples/zephyr/common/settings_autoload.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/settings/settings.h>
+
+static int settings_autoload(void)
+{
+    return settings_load();
+}
+
+SYS_INIT(settings_autoload, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
