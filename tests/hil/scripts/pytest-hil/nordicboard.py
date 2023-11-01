ec732be0fce66c560aa38ba08534f2a1edf1e6d3
Sam Friedman <sam@golioth.io>
2023-11-01 17:03:26 -0400

    tests: add program method to pytest hil fixture
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ tests/hil/scripts/pytest-hil/nordicboard.py
@@ -0,0 +1,14 @@
+from board import Board
+from pynrfjprog import HighLevel
+
+class NordicBoard(Board):
+    def program(self, fw_image):
+        with HighLevel.API() as api:
+            with HighLevel.DebugProbe(api, int(self.serial_number)) as probe:
+                program_options = HighLevel.ProgramOptions(
+                    erase_action=HighLevel.EraseAction.ERASE_ALL,
+                    reset=HighLevel.ResetAction.RESET_SYSTEM,
+                    verify=HighLevel.VerifyAction.VERIFY_READ,
+                )
+
+                probe.program(fw_image, program_options=program_options)
