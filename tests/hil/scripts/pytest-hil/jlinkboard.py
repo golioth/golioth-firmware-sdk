d12b168bda095852a41a9a00b5992b1d8a4c17a5
Sam Friedman <sam@golioth.io>
2023-11-06 14:54:44 -0500

    tests: add jlink board class to hil pytest fixture
    Add a `JLinkBoard` class that implements the `program()` method
    of the Board class to the pytest-hil fixture.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

---
+++ tests/hil/scripts/pytest-hil/jlinkboard.py
@@ -0,0 +1,16 @@
+import pylink
+
+from board import Board
+
+class JLinkBoard(Board):
+    def __init__(self, chip_name):
+        self.chip_name = chip_name
+
+    def program(self, fw_image):
+        jlink = pylink.JLink()
+        jlink.open(int(self.serial_number))
+        jlink.set_tif(pylink.JLinkInterfaces.SWD)
+        jlink.connect(self.chip_name)
+        jlink.reset()
+        jlink.flash_file(fw_image, 0)
+        jlink.reset(halt=False)
