fa27911df36e5f478926e1681ddedf675cee8e79
Sam Friedman <sam@golioth.io>
2023-12-14 18:01:08 -0500

    examples: esp-idf: remove magtag demo
    The hardware is out of stock, it's not one of our CVBs, and this kind
    of all-encompassing demo isn't in-line with our normal examples.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- examples/esp_idf/magtag_demo/main/light_sensor.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void light_sensor_init(void);
-uint32_t light_sensor_read_mV(void);
--- examples/esp_idf/magtag_demo/main/speaker.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void speaker_init(int dac_io, int enable_io);
-void speaker_play_audio(const uint8_t* audio, uint32_t audio_length, uint32_t sample_rate);
