1488eba01fcfd631809cc16f806590478f91f107
Sam Friedman <sam@golioth.io>
2023-12-14 17:02:43 -0500

    src: remove golioth_time module
    Previous commits removed the usaged of golioth_time_* APIs. Now
    we can remove the module itself.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- tests/unit_tests/fakes/golioth_time_fake.c
+++
@@ -1,5 +0,0 @@
-#include <fff.h>
-
-#include "golioth_time_fake.h"
-
-DEFINE_FAKE_VALUE_FUNC(uint64_t, golioth_time_millis);
--- tests/unit_tests/fakes/golioth_time_fake.h
+++
@@ -1,4 +0,0 @@
-#include <fff.h>
-#include <golioth_time.h>
-
-DECLARE_FAKE_VALUE_FUNC(uint64_t, golioth_time_millis);
