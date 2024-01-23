3b00f5e5e61bda1e1e7ebff1b3a494774d7e1f49
Marcin Niestroj <m.niestroj@emb.dev>
2024-01-23 15:54:59 +0100

    examples: zephyr: support authentication by TLS credential tag
    Introduce new Kconfig option for selecting which authentication mechanism
    should be used:
     * PSK
     * PKI
     * Tag
    
    When 'Tag' is selected, hardcoded_credentials module will just return tag
    configured with already existing CONFIG_GOLIOTH_COAP_CLIENT_CREDENTIALS_TAG
    Kconfig option.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- examples/zephyr/common/hardcoded_credentials.c
+++ examples/zephyr/common/hardcoded_credentials.c
@@ -9,7 +9,7 @@ LOG_MODULE_REGISTER(golioth_hardcoded_credentials, LOG_LEVEL_DBG);
 
 #include <golioth/client.h>
 
-#ifdef CONFIG_GOLIOTH_AUTH_METHOD_CERT
+#ifdef CONFIG_GOLIOTH_SAMPLE_AUTH_TYPE_PKI
 
 static const uint8_t tls_client_crt[] = {
 #include "golioth-systemclient-crt.inc"
@@ -23,7 +23,7 @@ static const uint8_t tls_ca_crt[] = {
 #include "golioth-systemclient-ca_crt.inc"
 };
 
-static const struct golioth_client_config _golioth_client_config_psk = {
+static const struct golioth_client_config _golioth_client_config = {
     .credentials =
         {
             .auth_type = GOLIOTH_TLS_AUTH_TYPE_PKI,
@@ -39,9 +39,9 @@ static const struct golioth_client_config _golioth_client_config_psk = {
         },
 };
 
-#else /* Using PSK Authentication */
+#elif defined(CONFIG_GOLIOTH_SAMPLE_AUTH_TYPE_PSK)
 
-static const struct golioth_client_config _golioth_client_config_psk = {
+static const struct golioth_client_config _golioth_client_config = {
     .credentials =
         {
             .auth_type = GOLIOTH_TLS_AUTH_TYPE_PSK,
@@ -54,9 +54,20 @@ static const struct golioth_client_config _golioth_client_config_psk = {
                 },
         },
 };
+
+#elif defined(CONFIG_GOLIOTH_SAMPLE_AUTH_TYPE_TAG)
+
+static const struct golioth_client_config _golioth_client_config = {
+    .credentials =
+        {
+            .auth_type = GOLIOTH_TLS_AUTH_TYPE_TAG,
+            .tag = CONFIG_GOLIOTH_COAP_CLIENT_CREDENTIALS_TAG,
+        },
+};
+
 #endif
 
 const struct golioth_client_config *golioth_sample_credentials_get(void)
 {
-    return &_golioth_client_config_psk;
+    return &_golioth_client_config;
 }
