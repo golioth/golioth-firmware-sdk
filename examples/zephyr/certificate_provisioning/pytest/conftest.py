68298d60de076229b4e6b9cefb25fc28eeaf6d58
Sam Friedman <sam@golioth.io>
2024-03-27 13:45:57 -0400

    examples: zephyr: cert_prov: add pytest args for wifi
    Standardize the basic set of arguments for all of the Zephyr
    example tests.
    
    Signed-off-by: Sam Friedman <sam@golioth.io>

--- examples/zephyr/certificate_provisioning/pytest/conftest.py
+++ examples/zephyr/certificate_provisioning/pytest/conftest.py
@@ -7,6 +7,8 @@ import subprocess
 def pytest_addoption(parser):
     parser.addoption("--device-port", type=str,
                      help="Device serial port path")
+    parser.addoption("--wifi-ssid", type=str, help="WiFi SSID")
+    parser.addoption("--wifi-psk",  type=str, help="WiFi PSK")
 
 @pytest.fixture(scope="session")
 def device_port(request):
@@ -42,3 +44,17 @@ async def device_name(project):
         await project.delete_device_by_name(name)
     except:
         pass
+
+@pytest.fixture(scope="session")
+def wifi_ssid(request):
+    if request.config.getoption("--wifi-ssid") is not None:
+        return request.config.getoption("--wifi-ssid")
+    else:
+        return os.environ['WIFI_SSID']
+
+@pytest.fixture(scope="session")
+def wifi_psk(request):
+    if request.config.getoption("--wifi-psk") is not None:
+        return request.config.getoption("--wifi-psk")
+    else:
+        return os.environ['WIFI_PSK']
