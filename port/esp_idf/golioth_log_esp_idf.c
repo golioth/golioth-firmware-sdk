fc247fa22106fb34fc562639d291993b40eb9c2d
Rashmi Vaidya <rashmi.vaidya72@gmail.com>
2024-04-23 10:31:18 -0700

    Improved ESP Log Backend with cloud logging option default to 0
    Signed-off-by: Rashmi Vaidya <rashmi.vaidya72@gmail.com>

--- port/esp_idf/golioth_log_esp_idf.c
+++ port/esp_idf/golioth_log_esp_idf.c
@@ -17,10 +17,6 @@ static struct golioth_client *_client = NULL;
 
 static int golioth_log_espidf(const char *format, va_list args)
 {
-
-    const char *new_format = format;
-    const char *tag_arg = NULL;
-
     if (!CONFIG_GOLIOTH_AUTO_LOG_TO_CLOUD)
     {
         return -1;
@@ -31,52 +27,50 @@ static int golioth_log_espidf(const char *format, va_list args)
         return -1;
     }
 
-    va_list new_args;
-    va_copy(new_args, args);
-
-    // Since the format contains time (%lu) and tag (%s) values that need not
-    // be sent to the cloud like so : "[0;32mI (%lu) %s: Actual Log %d [0m",
-    // we remove the string upto colon : and update the args list
-    // Also, the 7th character in the string tells the log level as
+    // The 7th character in the format tells the log level as
     // E = Error, W = Warning, I = info, D = Debug, V = Verbose
     char log_level = format[7];
-    const char *colon_pos = strchr(format, ':');
-    if (colon_pos != NULL)
-    {
-        new_format = colon_pos + 2;  // Skip ':' and space
 
-        // Move va_list to correspond to the new format
-        va_arg(new_args, unsigned long);           // Skip %lu
-        tag_arg = va_arg(new_args, const char *);  // Extract tag value
-    }
-
-    // Temporarily buffer to store the message
+    // Temporary buffer to store the message
     char msg_buffer[CONFIG_GOLIOTH_ESPLOG_MAX_BUFF_SIZE] = {0};
+    vsnprintf(msg_buffer, CONFIG_GOLIOTH_ESPLOG_MAX_BUFF_SIZE, format, args);
+    printf("%s", msg_buffer);
 
-    vsnprintf(msg_buffer, CONFIG_GOLIOTH_ESPLOG_MAX_BUFF_SIZE, new_format, new_args);
-
+    // The formatted string looks like this: "[0;32mI (%lu) %s: Actual Log %d [0m"
+    // The start and end characters convey formatting and color information
+    // This information should not be sent to the cloud.
     // Insert a null char 5 places behind to remove ESC[0m at the end of the string
-    msg_buffer[strlen(msg_buffer) - 5] = '\0';
     // Here strlen(msg_buffer) - 5 will not go out of bounds because the format will
     // always have at least 5 characters, even with an empty string
+    msg_buffer[strlen(msg_buffer) - 5] = '\0';
 
-    printf("%s\n", msg_buffer);
+    // Find ) and : characters in the string for extracting tag
+    char tag_str[64] = {0};
+    const char *tag = strchr(msg_buffer, ')');
+    const char *msg_str = strchr(msg_buffer, ':');
+    if (tag != NULL && msg_str != NULL)
+    {
+        tag = tag + 1;  // Skip space after )
+        strncpy(tag_str, tag, msg_str - tag);
+        // Ignore everything before ':'
+        msg_str = msg_str + 2;  // Skip ':' and space
+    }
 
     // Log to Golioth asynchronously.
     switch (log_level)
     {
         case 'E':
-            golioth_log_error_async(_client, tag_arg, msg_buffer, NULL, NULL);
+            golioth_log_error_async(_client, tag_str, msg_str, NULL, NULL);
             break;
         case 'W':
-            golioth_log_warn_async(_client, tag_arg, msg_buffer, NULL, NULL);
+            golioth_log_warn_async(_client, tag_str, msg_str, NULL, NULL);
             break;
         case 'I':
-            golioth_log_info_async(_client, tag_arg, msg_buffer, NULL, NULL);
+            golioth_log_info_async(_client, tag_str, msg_str, NULL, NULL);
             break;
         case 'V':  // fallthrough
         case 'D':
-            golioth_log_debug_async(_client, tag_arg, msg_buffer, NULL, NULL);
+            golioth_log_debug_async(_client, tag_str, msg_str, NULL, NULL);
             break;
         default:
             break;
@@ -98,7 +92,7 @@ vprintf_like_t golioth_log_reset_esp_vprintf(vprintf_like_t original_log_func)
 
 void golioth_sys_client_connected(void *client)
 {
-    if (CONFIG_GOLIOTH_AUTO_LOG_TO_CLOUD == 1)
+    if (CONFIG_GOLIOTH_ESPLOG_AUTO_LOG_TO_CLOUD == 1)
     {
         original_log_func = golioth_log_set_esp_vprintf(client);
     }
@@ -106,7 +100,7 @@ void golioth_sys_client_connected(void *client)
 
 void golioth_sys_client_disconnected(void *client)
 {
-    if (CONFIG_GOLIOTH_AUTO_LOG_TO_CLOUD == 1)
+    if (CONFIG_GOLIOTH_ESPLOG_AUTO_LOG_TO_CLOUD == 1)
     {
         golioth_log_reset_esp_vprintf(original_log_func);
     }
