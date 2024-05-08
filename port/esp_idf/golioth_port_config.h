6e6c7c2d10c4f7c3291938b10656a18ded20f30a
Rashmi Vaidya <rashmi.vaidya72@gmail.com>
2024-05-07 17:38:12 -0700

    ESP-IDF Log Backend - unrevert original commit
    Signed-off-by: Rashmi Vaidya <rashmi.vaidya72@gmail.com>

--- port/esp_idf/golioth_port_config.h
+++ port/esp_idf/golioth_port_config.h
@@ -34,33 +34,31 @@
         }                                                                    \
     } while (0)
 
-// TODO - should we hook into the ESP logging backend via esp_log_set_vprintf?
-// This would enable all logs to be sent to Golioth (not just the GLTH_LOGX logs).
-
-#define GLTH_LOGX(COLOR, LEVEL, LEVEL_STR, TAG, ...) \
-    do { \
-        if ((LEVEL) <= golioth_debug_get_log_level()) { \
-            uint64_t now_ms = golioth_sys_now_ms(); \
-            switch (LEVEL) { \
-                case GOLIOTH_DEBUG_LOG_LEVEL_ERROR: \
-                    ESP_LOGE(TAG, __VA_ARGS__); \
-                    break; \
-                case GOLIOTH_DEBUG_LOG_LEVEL_WARN: \
-                    ESP_LOGW(TAG, __VA_ARGS__); \
-                    break; \
-                case GOLIOTH_DEBUG_LOG_LEVEL_INFO: \
-                    ESP_LOGI(TAG, __VA_ARGS__); \
-                    break; \
-                case GOLIOTH_DEBUG_LOG_LEVEL_DEBUG: \
-                    ESP_LOGD(TAG, __VA_ARGS__); \
-                    break; \
+#define GLTH_LOGX(COLOR, LEVEL, LEVEL_STR, TAG, ...)  \
+    do                                                \
+    {                                                 \
+        if ((LEVEL) <= golioth_debug_get_log_level()) \
+        {                                             \
+            switch (LEVEL)                            \
+            {                                         \
+                case GOLIOTH_DEBUG_LOG_LEVEL_ERROR:   \
+                    ESP_LOGE(TAG, __VA_ARGS__);       \
+                    break;                            \
+                case GOLIOTH_DEBUG_LOG_LEVEL_WARN:    \
+                    ESP_LOGW(TAG, __VA_ARGS__);       \
+                    break;                            \
+                case GOLIOTH_DEBUG_LOG_LEVEL_INFO:    \
+                    ESP_LOGI(TAG, __VA_ARGS__);       \
+                    break;                            \
+                case GOLIOTH_DEBUG_LOG_LEVEL_DEBUG:   \
+                    ESP_LOGD(TAG, __VA_ARGS__);       \
+                    break;                            \
                 case GOLIOTH_DEBUG_LOG_LEVEL_VERBOSE: \
-                    ESP_LOGV(TAG, __VA_ARGS__); \
-                    break; \
-                case GOLIOTH_DEBUG_LOG_LEVEL_NONE: \
-                default: \
-                    break; \
-            } \
-            golioth_debug_printf(now_ms, LEVEL, TAG, __VA_ARGS__); \
-        } \
+                    ESP_LOGV(TAG, __VA_ARGS__);       \
+                    break;                            \
+                case GOLIOTH_DEBUG_LOG_LEVEL_NONE:    \
+                default:                              \
+                    break;                            \
+            }                                         \
+        }                                             \
     } while (0)
---
+++ port/modus_toolbox/golioth_sys_modus.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <golioth/golioth_sys.h>
+
+void golioth_sys_client_connected(void *client) {}
+
+void golioth_sys_client_disconnected(void *client) {}
