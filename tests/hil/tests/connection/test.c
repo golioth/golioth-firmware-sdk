6084a9d66c895600a560676be4a596da50379110
Mike Szczys <mike@golioth.io>
2024-07-30 15:27:30 -0500

    hil: connection: test golioth_client_destroy()
    - Add golioth_client_destroy() and subsequent reconnect to test ability to
      destroy and restart the client
    - Add a semaphore to block until each new connection is established.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- tests/hil/tests/connection/test.c
+++ tests/hil/tests/connection/test.c
@@ -3,22 +3,57 @@
 
 LOG_TAG_DEFINE(test_connect);
 
+static golioth_sys_sem_t _connected_sem = NULL;
+
+static void on_client_event(struct golioth_client *client,
+                            enum golioth_client_event event,
+                            void *arg)
+{
+    bool is_connected = (event == GOLIOTH_CLIENT_EVENT_CONNECTED);
+    if (is_connected)
+    {
+        golioth_sys_sem_give(_connected_sem);
+    }
+    GLTH_LOGI(TAG, "Golioth client %s", is_connected ? "connected" : "disconnected");
+}
+
 void hil_test_entry(const struct golioth_client_config *config)
 {
+    _connected_sem = golioth_sys_sem_create(1, 0);
+
     struct golioth_client *client = golioth_client_create(config);
 
-    (void) client;
+    golioth_client_register_event_callback(client, on_client_event, NULL);
+    golioth_sys_sem_take(_connected_sem, GOLIOTH_SYS_WAIT_FOREVER);
 
-    while (1)
-    {
-        golioth_sys_msleep(10 * 1000);
+    /* Pause to ensure we don't have out-of-order logs */
+    golioth_sys_msleep(1 * 1000);
 
-        GLTH_LOGI(TAG, "Stopping client");
-        golioth_client_stop(client);
+    GLTH_LOGI(TAG, "Stopping client");
+    golioth_client_stop(client);
 
-        golioth_sys_msleep(10 * 1000);
+    golioth_sys_msleep(10 * 1000);
 
-        GLTH_LOGI(TAG, "Starting client");
-        golioth_client_start(client);
-    }
+    GLTH_LOGI(TAG, "Starting client");
+    golioth_client_start(client);
+
+    golioth_sys_sem_take(_connected_sem, GOLIOTH_SYS_WAIT_FOREVER);
+
+    /* Pause to ensure we don't have out-of-order logs */
+    golioth_sys_msleep(1 * 1000);
+
+    GLTH_LOGI(TAG, "Destroying client");
+    golioth_client_destroy(client);
+    client = NULL;
+
+    golioth_sys_msleep(10 * 1000);
+
+    GLTH_LOGI(TAG, "Starting client");
+    client = golioth_client_create(config);
+    golioth_client_start(client);
+
+    golioth_sys_sem_take(_connected_sem, GOLIOTH_SYS_WAIT_FOREVER);
+
+    /* Pause to ensure logs are show */
+    golioth_sys_msleep(1 * 1000);
 }
