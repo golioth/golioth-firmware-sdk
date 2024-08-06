dd2294fd82c5b0b89d77ef09604e0b3722fcacdf
Mike Szczys <mike@golioth.io>
2024-08-06 13:18:09 -0400

    coap: zephyr: configure blocksize for blockwise download
    Populate the blocksize of coap block2 using
    GOLIOTH_BLOCKWISE_DOWNLOAD_MAX_BLOCK_SIZE
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/coap_client_zephyr.c
+++ src/coap_client_zephyr.c
@@ -381,6 +381,7 @@ static int golioth_coap_get_block(golioth_coap_request_msg_t *req)
     }
 
     coap_req->block_ctx.current = (req->get_block.block_index * req->get_block.block_size);
+    coap_req->block_ctx.block_size = coap_bytes_to_block_size(req->get_block.block_size);
 
     err = golioth_coap_req_append_block2_option(coap_req);
     if (err)
