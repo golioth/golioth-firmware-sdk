e0b0d068ce513a6faf0b30313be13c2b4b7f2628
Mike Szczys <mike@golioth.io>
2024-08-21 17:25:35 -0500

    port: esp-idf: run clang-format on files
    No substantive changes were made in this commit. It merely applies the
    clang-format rules to these files.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- port/esp_idf/fw_update_esp_idf.c
+++ port/esp_idf/fw_update_esp_idf.c
@@ -15,58 +15,66 @@
 #define TAG "fw_update_esp_idf"
 
 static esp_ota_handle_t _update_handle;
-static const esp_partition_t* _update_partition;
+static const esp_partition_t *_update_partition;
 
-bool fw_update_is_pending_verify(void) {
-    const esp_partition_t* running = esp_ota_get_running_partition();
+bool fw_update_is_pending_verify(void)
+{
+    const esp_partition_t *running = esp_ota_get_running_partition();
     esp_ota_img_states_t ota_state;
-    if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK) {
-        if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+    if (esp_ota_get_state_partition(running, &ota_state) == ESP_OK)
+    {
+        if (ota_state == ESP_OTA_IMG_PENDING_VERIFY)
+        {
             return true;
         }
     }
     return false;
 }
 
-void fw_update_rollback(void) {
+void fw_update_rollback(void)
+{
     esp_ota_mark_app_invalid_rollback_and_reboot();
 }
 
-void fw_update_reboot(void) {
+void fw_update_reboot(void)
+{
     esp_restart();
 }
 
-void fw_update_cancel_rollback(void) {
+void fw_update_cancel_rollback(void)
+{
     esp_ota_mark_app_valid_cancel_rollback();
 }
 
-enum golioth_status fw_update_handle_block(
-        const uint8_t* block,
-        size_t block_size,
-        size_t offset,
-        size_t total_size) {
+enum golioth_status fw_update_handle_block(const uint8_t *block,
+                                           size_t block_size,
+                                           size_t offset,
+                                           size_t total_size)
+{
     esp_err_t err = ESP_OK;
 
-    if (offset == 0) {
+    if (offset == 0)
+    {
         _update_partition = esp_ota_get_next_update_partition(NULL);
         assert(_update_partition);
-        GLTH_LOGI(
-                TAG,
-                "Writing to partition subtype %d at offset 0x%" PRIx32,
-                _update_partition->subtype,
-                _update_partition->address);
+        GLTH_LOGI(TAG,
+                  "Writing to partition subtype %d at offset 0x%" PRIx32,
+                  _update_partition->subtype,
+                  _update_partition->address);
 
         GLTH_LOGI(TAG, "Erasing flash");
         err = esp_ota_begin(_update_partition, OTA_SIZE_UNKNOWN, &_update_handle);
-        if (err != ESP_OK) {
+        if (err != ESP_OK)
+        {
             GLTH_LOGE(TAG, "esp_ota_begin failed (%s)", esp_err_to_name(err));
             esp_ota_abort(_update_handle);
             return GOLIOTH_ERR_FAIL;
         }
     }
 
-    err = esp_ota_write(_update_handle, (const void*)block, block_size);
-    if (err != ESP_OK) {
+    err = esp_ota_write(_update_handle, (const void *) block, block_size);
+    if (err != ESP_OK)
+    {
         GLTH_LOGE(TAG, "esp_ota_write failed (%s)", esp_err_to_name(err));
         esp_ota_abort(_update_handle);
         return GOLIOTH_ERR_FAIL;
@@ -75,17 +83,23 @@ enum golioth_status fw_update_handle_block(
     return GOLIOTH_OK;
 }
 
-void fw_update_post_download(void) {
+void fw_update_post_download(void)
+{
     // Nothing to do
 }
 
-enum golioth_status fw_update_validate(void) {
+enum golioth_status fw_update_validate(void)
+{
     assert(_update_handle);
     esp_err_t err = esp_ota_end(_update_handle);
-    if (err != ESP_OK) {
-        if (err == ESP_ERR_OTA_VALIDATE_FAILED) {
+    if (err != ESP_OK)
+    {
+        if (err == ESP_ERR_OTA_VALIDATE_FAILED)
+        {
             GLTH_LOGE(TAG, "Image validation failed, image is corrupted");
-        } else {
+        }
+        else
+        {
             GLTH_LOGE(TAG, "esp_ota_end failed (%s)!", esp_err_to_name(err));
         }
 
@@ -95,20 +109,24 @@ enum golioth_status fw_update_validate(void) {
     return GOLIOTH_OK;
 }
 
-enum golioth_status fw_update_change_boot_image(void) {
+enum golioth_status fw_update_change_boot_image(void)
+{
     assert(_update_partition);
 
     GLTH_LOGI(TAG, "Setting boot partition");
     esp_err_t err = esp_ota_set_boot_partition(_update_partition);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         GLTH_LOGE(TAG, "esp_ota_set_boot_partition failed (%s)!", esp_err_to_name(err));
         return GOLIOTH_ERR_FAIL;
     }
     return GOLIOTH_OK;
 }
 
-void fw_update_end(void) {
-    if (_update_handle) {
+void fw_update_end(void)
+{
+    if (_update_handle)
+    {
         esp_ota_end(_update_handle);
     }
 }
