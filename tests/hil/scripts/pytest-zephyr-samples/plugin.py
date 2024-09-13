57cf4c2abc7efe77b133f6c7f8139407bcb4f7e8
Mike Szczys <mike@golioth.io>
2024-09-13 17:21:35 -0500

    hil: zephyr: samples: add allure reports
    - Add Allure reports customization to the pytest-zephyr-samples plugin to
    allow input of runner-name/hil-board/platform name and generate report for
    each test function that runs.
    - Add pytest options for allure reports to Zephyr samples workflow.
    - Add pytest options for allure reports to native sim (nsim) samples in the
      hil_tests workflow.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- tests/hil/scripts/pytest-zephyr-samples/plugin.py
+++ tests/hil/scripts/pytest-zephyr-samples/plugin.py
@@ -1,9 +1,12 @@
 import pytest
+import allure
 import os
 
 def pytest_addoption(parser):
     parser.addoption("--wifi-ssid",   type=str, help="WiFi SSID")
     parser.addoption("--wifi-psk",    type=str, help="WiFi PSK")
+    parser.addoption("--hil-board",   type=str, help="Simple board name")
+    parser.addoption("--runner-name", type=str, help="Self-hosted runner name")
 
 @pytest.fixture(scope="session")
 def wifi_ssid(request):
@@ -18,3 +21,33 @@ def wifi_psk(request):
         return request.config.getoption("--wifi-psk")
     else:
         return os.environ.get('WIFI_PSK')
+
+@pytest.fixture(scope="session")
+def hil_board(request):
+    if request.config.getoption("--hil-board") is not None:
+        return request.config.getoption("--hil-board")
+    else:
+        return os.environ['hil_board']
+
+@pytest.fixture(scope="session")
+def runner_name(request):
+    return request.config.getoption("--runner-name")
+
+@pytest.fixture(autouse=True, scope="session")
+def add_allure_report_parent_suite(hil_board):
+    # Set the full Allure suite name in case there is a failure during a fixture (before a test
+    # function runs).
+    allure.dynamic.parent_suite(f"hil.zephyr.{hil_board}")
+
+@pytest.fixture(autouse=True, scope="function")
+def add_allure_report_device_and_platform(hil_board, runner_name):
+    # Set the Allure information for every test function.
+    # Especially important are the parameters which identify variants of the same test
+    allure.dynamic.tag(hil_board)
+    allure.dynamic.tag("zephyr")
+    allure.dynamic.parameter("board_name", hil_board)
+    allure.dynamic.parameter("platform_name", "zephyr")
+    allure.dynamic.parent_suite(f"hil.zephyr.{hil_board}")
+
+    if runner_name is not None:
+        allure.dynamic.tag(runner_name)
