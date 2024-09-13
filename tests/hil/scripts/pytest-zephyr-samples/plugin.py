9cf9151d3fd77116925ae5bbd2902a2a326093c0
Mike Szczys <mike@golioth.io>
2024-09-13 14:55:50 -0500

    hil: zephyr: samples: add plugin for common test fixtures
    Add plugin that makes WiFi credentials available as pytest options along
    with fixtures to make them available to tests.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ tests/hil/scripts/pytest-zephyr-samples/plugin.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+
+def pytest_addoption(parser):
+    parser.addoption("--wifi-ssid",   type=str, help="WiFi SSID")
+    parser.addoption("--wifi-psk",    type=str, help="WiFi PSK")
+
+@pytest.fixture(scope="session")
+def wifi_ssid(request):
+    if request.config.getoption("--wifi-ssid") is not None:
+        return request.config.getoption("--wifi-ssid")
+    else:
+        return os.environ.get('WIFI_SSID')
+
+@pytest.fixture(scope="session")
+def wifi_psk(request):
+    if request.config.getoption("--wifi-psk") is not None:
+        return request.config.getoption("--wifi-psk")
+    else:
+        return os.environ.get('WIFI_PSK')
