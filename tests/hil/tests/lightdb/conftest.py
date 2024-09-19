0666b61f5b7942fb8217bc8347fcfe3f33a76b3c
Mike Szczys <mike@golioth.io>
2024-09-19 14:41:35 -0500

    hil: add junit suite names
    Adding suite names to the configuration allows us to group all the
    integration tests into a single suite for each platform/device on the
    summary.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- tests/hil/tests/lightdb/conftest.py
+++ tests/hil/tests/lightdb/conftest.py
@@ -1,5 +1,10 @@
 import pytest
+import os
 
 @pytest.fixture(scope='session')
 def anyio_backend():
     return 'trio'
+
+def pytest_configure(config):
+    suite_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    config.inicfg["junit_suite_name"] = suite_name
