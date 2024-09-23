105746a69a8a00de3389ba477721975913d4f279
Mike Szczys <mike@golioth.io>
2024-09-23 17:13:47 -0500

    hil: add rak5010 to pytest-hil plugin
    Add handling of the rak5010 to the pytest-hil plugin. To use this board
    with pytest, pass the port for the BlackMagicProbe programmer using the
    `--bmp-port` flag or set the board as a CI_BLACKMAGICPROBE_PORT environment
    variable.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ tests/hil/scripts/pytest-hil/rak5010.py
@@ -0,0 +1,14 @@
+from zephyrboard import ZephyrBoard
+from bmpboard import BMPBoard
+from time import sleep
+
+class RAK5010(ZephyrBoard, BMPBoard):
+    def program(self, *args, **kwargs):
+        BMPBoard.program(self, *args, **kwargs)
+
+        # Arbitrary sleep while the software-based USB serial becomes available
+        sleep(3)
+
+    @property
+    def USES_WIFI(self):
+        return False
