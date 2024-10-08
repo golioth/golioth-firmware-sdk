15a5c4bead24561d5e36553b944736f5be5a9abe
Mike Szczys <mike@golioth.io>
2024-10-08 14:14:29 -0500

    coap_blockwise: add block_idx param to golioth_blockwise_get()
    Allow blockwise_get to start from a non-zero block number.
    
    - *block_idx (input): first block number to request from server
    - *block_idx (output): upon return, the value will be set to the next block
      number expected. Use as input to resume block download.
    - *block_idx == NULL: start from block_idx 0, do not return a block_idx
      value
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/coap_blockwise.h
+++ src/coap_blockwise.h
@@ -34,9 +34,18 @@ typedef enum golioth_status (*write_block_cb)(uint32_t block_idx,
                                               size_t negotiated_block_size,
                                               void *callback_arg);
 
+/* Begin a blockwise download from a given block index
+ *
+ * This function will block until the download finishes or an error has occurred.
+ * - The block_idx parameter out will be set to the next expected block.
+ * - This value may be used as input to resume after a block download has failed.
+ * - block_idx may be NULL, in which case 0 will be used for first block_idx and no value will be
+ *   passed out.
+ */
 enum golioth_status golioth_blockwise_get(struct golioth_client *client,
                                           const char *path_prefix,
                                           const char *path,
                                           enum golioth_content_type content_type,
+                                          uint32_t *block_idx,
                                           write_block_cb cb,
                                           void *callback_arg);
