4e32776bc9949d2822b0f1410bf98e86530afa7e
Mike Szczys <mike@golioth.io>
2024-10-10 17:29:07 -0500

    golioth_status: add GOLIOTH_ERR_INVALID_BLOCK_SIZE
    Add GOLIOTH_ERR_INVALID_BLOCK_SIZE to indicate that a blocksize supplied by
    the read or write callback was invalid.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- include/golioth/golioth_status.h
+++ include/golioth/golioth_status.h
@@ -12,23 +12,25 @@ extern "C"
 #pragma once
 
 /// Status code enum used throughout Golioth SDK
-#define FOREACH_GOLIOTH_STATUS(STATUS)      \
-    STATUS(GOLIOTH_OK) /* 0 */              \
-    STATUS(GOLIOTH_ERR_FAIL)                \
-    STATUS(GOLIOTH_ERR_DNS_LOOKUP)          \
-    STATUS(GOLIOTH_ERR_NOT_IMPLEMENTED)     \
-    STATUS(GOLIOTH_ERR_MEM_ALLOC)           \
-    STATUS(GOLIOTH_ERR_NULL) /* 5 */        \
-    STATUS(GOLIOTH_ERR_INVALID_FORMAT)      \
-    STATUS(GOLIOTH_ERR_SERIALIZE)           \
-    STATUS(GOLIOTH_ERR_IO)                  \
-    STATUS(GOLIOTH_ERR_TIMEOUT)             \
-    STATUS(GOLIOTH_ERR_QUEUE_FULL) /* 10 */ \
-    STATUS(GOLIOTH_ERR_NOT_ALLOWED)         \
-    STATUS(GOLIOTH_ERR_INVALID_STATE)       \
-    STATUS(GOLIOTH_ERR_NO_MORE_DATA)        \
-    STATUS(GOLIOTH_ERR_NACK)                \
-    STATUS(GOLIOTH_ERR_BAD_REQUEST)
+#define FOREACH_GOLIOTH_STATUS(STATUS)       \
+    STATUS(GOLIOTH_OK) /* 0 */               \
+    STATUS(GOLIOTH_ERR_FAIL)                 \
+    STATUS(GOLIOTH_ERR_DNS_LOOKUP)           \
+    STATUS(GOLIOTH_ERR_NOT_IMPLEMENTED)      \
+    STATUS(GOLIOTH_ERR_MEM_ALLOC)            \
+    STATUS(GOLIOTH_ERR_NULL) /* 5 */         \
+    STATUS(GOLIOTH_ERR_INVALID_FORMAT)       \
+    STATUS(GOLIOTH_ERR_SERIALIZE)            \
+    STATUS(GOLIOTH_ERR_IO)                   \
+    STATUS(GOLIOTH_ERR_TIMEOUT)              \
+    STATUS(GOLIOTH_ERR_QUEUE_FULL) /* 10 */  \
+    STATUS(GOLIOTH_ERR_NOT_ALLOWED)          \
+    STATUS(GOLIOTH_ERR_INVALID_STATE)        \
+    STATUS(GOLIOTH_ERR_NO_MORE_DATA)         \
+    STATUS(GOLIOTH_ERR_NACK)                 \
+    STATUS(GOLIOTH_ERR_BAD_REQUEST) /* 15 */ \
+    STATUS(GOLIOTH_ERR_INVALID_BLOCK_SIZE)
+
 
 #define GENERATE_GOLIOTH_STATUS_ENUM(code) code,
 enum golioth_status
