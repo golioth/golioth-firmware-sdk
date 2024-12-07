02eb88a043bfdd2ed67567515c48dbd78c08f5a6
Mike Szczys <mike@golioth.io>
2024-12-07 15:47:17 -0600

    samples: zephyr: rpc-query: remove query, add repeat_word
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/zephyr/rpc-query/src/main.c
+++ examples/zephyr/rpc-query/src/main.c
@@ -16,6 +16,8 @@ LOG_MODULE_REGISTER(rpc_sample, LOG_LEVEL_DBG);
 
 static K_SEM_DEFINE(connected, 0, 1);
 
+#define RPC_RETURN_STRING_BUF_SIZE 128
+
 static enum golioth_rpc_status on_multiply(zcbor_state_t *request_params_array,
                                            zcbor_state_t *response_detail_map,
                                            void *callback_arg)
@@ -47,20 +49,47 @@ static enum golioth_rpc_status on_multiply(zcbor_state_t *request_params_array,
     return GOLIOTH_RPC_OK;
 }
 
-static enum golioth_rpc_status on_query(zcbor_state_t *request_params_array,
+static enum golioth_rpc_status on_repeat_word(zcbor_state_t *request_params_array,
                                         zcbor_state_t *response_detail_map,
                                         void *callback_arg)
 {
-    bool ok = zcbor_tstr_put_lit(response_detail_map, "multiply")
-        && zcbor_float64_put(response_detail_map, 0x321)
-        && zcbor_tstr_put_lit(response_detail_map, "reboot")
-        && zcbor_float64_put(response_detail_map, 0x0);
+    struct zcbor_string zstr;
+    double multiple_f;
+    bool ok;
+
+    ok = zcbor_tstr_decode(request_params_array, &zstr)
+        && zcbor_float64_decode(request_params_array, &multiple_f);
 
     if (!ok)
     {
-        LOG_ERR("Failed to encode value");
-        return GOLIOTH_RPC_RESOURCE_EXHAUSTED;
+        LOG_ERR("Could not decode params");
+        return GOLIOTH_RPC_INVALID_ARGUMENT;
     }
+
+    char return_str[RPC_RETURN_STRING_BUF_SIZE] = { 0 };
+    uint32_t multiple = (uint32_t) multiple_f;
+    size_t seed_len = zstr.len + 1; /* Add one for space */
+
+    if (multiple * seed_len > RPC_RETURN_STRING_BUF_SIZE)
+    {
+        LOG_ERR("Input params too large to return");
+        return GOLIOTH_RPC_INVALID_ARGUMENT;
+    }
+
+    for (uint32_t i = 0; i < multiple; i++)
+    {
+        int offset = seed_len * i;
+        int remaining = RPC_RETURN_STRING_BUF_SIZE - offset;
+        if (remaining >= seed_len)
+        {
+            snprintk(return_str + offset, seed_len + 1, "%.*s ", zstr.len, zstr.value);
+        }
+    }
+
+    return_str[(seed_len * multiple) - 1] = '\0';
+
+    ok = zcbor_tstr_put_lit(response_detail_map, "response")
+        && zcbor_tstr_encode_ptr(response_detail_map, return_str, strlen(return_str));
 
     return GOLIOTH_RPC_OK;
 }
@@ -96,16 +125,15 @@ int main(void)
 
     k_sem_take(&connected, K_FOREVER);
 
-
-    // This is required for the demo, but might not be required long term and the callback 
-    // is captured by the SDK
-    golioth_rpc_register(rpc, "query", on_query, NULL);
-
-
     // Register the rpc itself and the parameters individually
     int err = golioth_rpc_register(rpc, "multiply", on_multiply, NULL);
     golioth_rpc_add_param_info(rpc,"multiply", "int_a",GOLIOTH_RPC_PARAM_TYPE_NUM);
     golioth_rpc_add_param_info(rpc,"multiply", "int_b",GOLIOTH_RPC_PARAM_TYPE_NUM);
+
+    // Register the rpc itself and the parameters individually
+    err = golioth_rpc_register(rpc, "repeat_word", on_repeat_word, NULL);
+    golioth_rpc_add_param_info(rpc,"repeat_word", "word",GOLIOTH_RPC_PARAM_TYPE_STR);
+    golioth_rpc_add_param_info(rpc,"repeat_word", "multiple",GOLIOTH_RPC_PARAM_TYPE_NUM);
 
     if (err)
     {
