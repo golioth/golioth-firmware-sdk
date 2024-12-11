ed26ff752e66e2517a9d415aaea1d63bfc8cf6fc
Mike Szczys <mike@golioth.io>
2024-12-11 16:24:52 -0600

    port: zephyr: use Zephyr random for golioth_sys_rand/srand functions
    Zephyr random module connects to hardware-based randomness sources. Enable
    this for use with CoAP token generator.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- port/zephyr/include/golioth_port_config.h
+++ port/zephyr/include/golioth_port_config.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <zephyr/logging/log.h>
+#include <zephyr/random/random.h>
 
 #define LOG_TAG_DEFINE(tag) LOG_MODULE_REGISTER(tag, CONFIG_GOLIOTH_LOG_LEVEL)
 
@@ -25,3 +26,8 @@
 #define GLTH_LOG_BUFFER_HEXDUMP(tag, buf, size, level) LOG_HEXDUMP_DBG(buf, size, "buffer")
 
 #endif
+
+/* Use Zephyr random subsystem, which has support for HW RNGs and HW entropy sources. Seeding is
+ * taken care of by Zephyr automatically, so make srand() a noop. */
+#define golioth_sys_srand(seed) (void) (seed)
+#define golioth_sys_rand() sys_rand32_get()
