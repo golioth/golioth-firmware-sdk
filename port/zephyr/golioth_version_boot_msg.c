fb36763fa6ce4fc461f5599f7270f527ae0b793e
Mike Szczys <mike@golioth.io>
2024-12-17 23:21:50 -0600

    zephyr: add option to display Golioth Firmware SDK version at boot
    Use CONFIG_GOLIOTH_SHOW_VERSION_BOOT_MSG symbol (enabled by default) to
    disable. GOLIOTH_VERSION_INCLUDE_COMMIT (enabled by default) allows the
    commit hash to be removed from the version number.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

---
+++ port/zephyr/golioth_version_boot_msg.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <golioth_version.h>
+
+#define GOLTIOTH_BOOT_BANNER_PRIO 32
+int golioth_boot_banner(void)
+{
+    printk("*** Golioth Firmware SDK %s ***\n",
+           (IS_ENABLED(CONFIG_GOLIOTH_VERSION_INCLUDE_COMMIT)) ? STRINGIFY(GOLIOTH_BUILD_VERSION)
+                                                               : "v" GOLIOTH_VERSION_STRING);
+
+    return 0;
+}
+
+SYS_INIT(golioth_boot_banner, APPLICATION, GOLTIOTH_BOOT_BANNER_PRIO);
