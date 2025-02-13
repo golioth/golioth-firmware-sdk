761fa28f9695dd16b552d725f4401927a2441637
Mike Szczys <mike@golioth.io>
2025-02-13 12:48:04 -0600

    stream: add multipart upload
    - Call `golioth_stream_set_blockwise_start()` to create a context.
    - Call `golioth_stream_set_blockwise_send()` for each block
    - Call `golioth_stream_set_blockwise_finish()` to destroy the context.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/stream.c
+++ src/stream.c
@@ -73,5 +73,36 @@ enum golioth_status golioth_stream_set_blockwise_sync(struct golioth_client *cli
                                   arg);
 }
 
+struct blockwise_transfer *golioth_stream_blockwise_start(struct golioth_client *client,
+                                                          const char *path,
+                                                          enum golioth_content_type content_type)
+{
+    return golioth_blockwise_upload_start(client, GOLIOTH_STREAM_PATH_PREFIX, path, content_type);
+}
+
+void golioth_stream_blockwise_finish(struct blockwise_transfer *ctx)
+{
+    return golioth_blockwise_upload_finish(ctx);
+}
+
+enum golioth_status golioth_stream_blockwise_set_block_async(struct blockwise_transfer *ctx,
+                                                             uint32_t block_idx,
+                                                             const uint8_t *block_buffer,
+                                                             size_t data_len,
+                                                             bool is_last,
+                                                             golioth_set_block_cb_fn callback,
+                                                             void *callback_arg)
+{
+    return golioth_blockwise_upload_block(ctx,
+                                          block_idx,
+                                          block_buffer,
+                                          data_len,
+                                          is_last,
+                                          callback,
+                                          callback_arg,
+                                          false,
+                                          GOLIOTH_SYS_WAIT_FOREVER);
+}
+
 
 #endif  // CONFIG_GOLIOTH_STREAM
