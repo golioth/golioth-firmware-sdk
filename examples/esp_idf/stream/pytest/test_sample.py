54dfc22f0b50d4e7e1b965bdc9bd54aff57ed17e
Mike Szczys <mike@golioth.io>
2025-10-23 14:09:33 -0500

    CI: espidf: stream: replace websocket with get
    use a get command instead of the websocket-based iter command with is being
    deprecated from the python-golioth-tools.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/esp_idf/stream/pytest/test_sample.py
+++ examples/esp_idf/stream/pytest/test_sample.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import contextlib
+import datetime
 import logging
 
 import pytest
@@ -26,9 +26,25 @@ async def test_stream(board, device):
 
     lower_temp = 20.0
     upper_temp = 21.5
-    async with contextlib.aclosing(device.stream.iter()) as stream_iter:
-        async for value in stream_iter:
-            LOGGER.info("ts: {0}, temp: {1}".format(value['timestamp'], value['data']['temp']))
-            assert (value["data"]["temp"] >= lower_temp and value["data"]["temp"] <= upper_temp)
-            if value["data"]["temp"] == upper_temp:
-                break
+
+    start = datetime.datetime.now(datetime.UTC)
+    await board.wait_for_regex_in_line(
+        ".*Sending temperature {}.".format(str(upper_temp)), timeout_s=90.0
+    )
+
+    await trio.sleep(2) # Give stream time to hit server
+    end = datetime.datetime.now(datetime.UTC)
+
+    stream_data = await device.stream.get(path = 'temp',
+                                          start = start.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                                          end = end.strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+
+    temp = lower_temp
+    for value in reversed(stream_data["list"]):
+        LOGGER.info("ts: {0}, temp: {1}".format(value["time"], value["data"]["temp"]))
+        assert (value["data"]["temp"] == temp)
+
+        if temp == upper_temp:
+            break
+
+        temp += 0.5
