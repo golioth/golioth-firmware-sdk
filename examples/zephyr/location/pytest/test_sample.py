2beb1d1d6adb2201ff23b8c90ad9a7f8e9ede65d
Mike Szczys <mike@golioth.io>
2025-10-24 13:18:14 -0500

    CI: zephyr: location: replace websocket with get
    use a get command instead of the websocket-based iter command with is being
    deprecated from the python-golioth-tools.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/zephyr/location/pytest/test_sample.py
+++ examples/zephyr/location/pytest/test_sample.py
@@ -2,12 +2,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import contextlib
+import datetime
 
 from twister_harness.device.device_adapter import DeviceAdapter
 from twister_harness.helpers.shell import Shell
 
 import pytest
+import trio
 
 pytestmark = pytest.mark.anyio
 
@@ -19,12 +20,25 @@ async def test_location(shell: Shell, dut: DeviceAdapter, device):
     shell.exec_command(f"settings set golioth/psk-id {golioth_cred.identity}")
     shell.exec_command(f"settings set golioth/psk {golioth_cred.key}")
 
+
     # Wait for Golioth connection
 
+    start = datetime.datetime.now(datetime.UTC)
     dut.readlines_until(regex=".*Golioth CoAP client connected", timeout=90.0)
 
+    # Wait for device to send location
+
+    dut.readlines_until(regex=".*Successfully streamed network data", timeout=90.0)
+
     # Verify network info data
-    async with contextlib.aclosing(device.stream.iter()) as stream_iter:
-        async for value in stream_iter:
-            assert "cell" in value["data"] or "wifi" in value["data"]
-            break
+
+    await trio.sleep(2) # Give stream time to hit server
+    end = datetime.datetime.now(datetime.UTC)
+
+    stream_data = await device.stream.get(path = '',
+                                          start = start.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+                                          end = end.strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+
+    for value in reversed(stream_data["list"]):
+        assert "cell" in value["data"] or "wifi" in value["data"]
+        break
