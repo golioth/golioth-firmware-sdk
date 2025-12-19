9fb3f27bc25bf1e619336e8055ef355e59c48385
Mike Szczys <mike@golioth.io>
2025-12-19 12:52:19 -0600

    CI: esp-idf: add polling when fetching logs for cpp sample
    poll until fetched log count matches the expected number of entries.
    Polling includes a timeout and will assert.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- examples/esp_idf/cpp/pytest/test_sample.py
+++ examples/esp_idf/cpp/pytest/test_sample.py
@@ -1,7 +1,11 @@
 import logging
 import pytest
-from datetime import datetime, timezone
+import datetime
 import re
+import trio
+
+LOG_FETCH_POLLING_LIMIT = datetime.timedelta(seconds=60)
+LOG_EXPECTED_COUNT = 2
 
 pytestmark = pytest.mark.anyio
 
@@ -11,12 +15,33 @@ async def test_hello(board, device):
     await board.set_golioth_psk_credentials(golioth_cred.identity, golioth_cred.key)
 
     # Record timestamp and wait for fourth hello message
-    start = datetime.now(timezone.utc)
+    start = datetime.datetime.now(datetime.UTC)
     await board.wait_for_regex_in_line('.*Hello, Golioth 2!', timeout_s=90.0)
 
-    # Check logs for hello messages
-    end = datetime.now(timezone.utc)
-    logs = await device.get_logs({'start': start.strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'end': end.strftime('%Y-%m-%dT%H:%M:%S.%fZ')})
+    # Fetch logs
+
+    log_fetch_start = datetime.datetime.now(datetime.UTC)
+
+    while True:
+        end = datetime.datetime.now(datetime.UTC)
+
+        logs = await device.get_logs(
+            {
+                "start": start.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "end": end.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "module": "app_main",
+            }
+        )
+
+        if len(logs) >= LOG_EXPECTED_COUNT:
+            break
+
+        assert (
+            LOG_FETCH_POLLING_LIMIT
+            > datetime.datetime.now(datetime.UTC) - log_fetch_start
+        )
+
+        await trio.sleep(1)
 
     # Test logs received from server
     r = re.compile(".*Hello, Golioth 1!")
