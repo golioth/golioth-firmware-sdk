a838cbc5cada8244aa807df28c652e0e3b3376da
Marcin Niestroj <m.niestroj@emb.dev>
2026-01-15 10:10:20 +0100

    bump NCS to 3.2.1
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- examples/zephyr/common/nrf91_lte_monitor.c
+++ examples/zephyr/common/nrf91_lte_monitor.c
@@ -23,23 +23,44 @@ static void lte_handler(const struct lte_lc_evt *const evt)
                 case LTE_LC_NW_REG_NOT_REGISTERED:
                     LOG_INF("Network: Not registered");
                     break;
+                case LTE_LC_NW_REG_NO_SUITABLE_CELL:
+                    LOG_INF("Network: No suitable cell");
+                    break;
                 case LTE_LC_NW_REG_REGISTERED_HOME:
                     LOG_INF("Network: Registered (home)");
-                    break;
-                case LTE_LC_NW_REG_SEARCHING:
-                    LOG_INF("Network: Searching");
-                    break;
-                case LTE_LC_NW_REG_REGISTRATION_DENIED:
-                    LOG_INF("Network: Registration denied");
-                    break;
-                case LTE_LC_NW_REG_UNKNOWN:
-                    LOG_INF("Network: Unknown");
                     break;
                 case LTE_LC_NW_REG_REGISTERED_ROAMING:
                     LOG_INF("Network: Registered (roaming)");
                     break;
+                case LTE_LC_NW_REG_REGISTRATION_DENIED:
+                    LOG_INF("Network: Registration denied");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_NOT_REGISTERED:
+                    LOG_INF("Network: RX only, not registered");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_REGISTERED_HOME:
+                    LOG_INF("Network: RX only, registered (home)");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_REGISTERED_ROAMING:
+                    LOG_INF("Network: RX only, registered (roaming)");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_REGISTRATION_DENIED:
+                    LOG_INF("Network: RX only, registration denied");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_SEARCHING:
+                    LOG_INF("Network: RX only, searching");
+                    break;
+                case LTE_LC_NW_REG_RX_ONLY_UNKNOWN:
+                    LOG_INF("Network: RX only, unknown");
+                    break;
+                case LTE_LC_NW_REG_SEARCHING:
+                    LOG_INF("Network: Searching");
+                    break;
                 case LTE_LC_NW_REG_UICC_FAIL:
                     LOG_INF("Network: UICC fail");
+                    break;
+                case LTE_LC_NW_REG_UNKNOWN:
+                    LOG_INF("Network: Unknown");
                     break;
             }
             break;
@@ -55,7 +76,7 @@ static void lte_handler(const struct lte_lc_evt *const evt)
             }
             break;
         case LTE_LC_EVT_MODEM_EVENT:
-            switch (evt->modem_evt)
+            switch (evt->modem_evt.type)
             {
                 case LTE_LC_MODEM_EVT_RESET_LOOP:
                     LOG_INF("Modem: Reset Loop detected");
